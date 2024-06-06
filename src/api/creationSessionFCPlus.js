221b14b58314a511ed712d1f4d3997840054cb07
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-06-06 13:50:25 +0200

    [NETTOYAGE] Passe fonction anonyme plutôt qu'appel

--- src/api/creationSessionFCPlus.js
+++ src/api/creationSessionFCPlus.js
@@ -16,7 +16,7 @@ const creationSessionFCPlus = (config, requete, reponse) => {
     .then((url) => `${url}?scope=profile%20openid%20birthcountry%20birthplace&acr_values=eidas2&claims={%22id_token%22:{%22amr%22:{%22essential%22:true}}}&prompt=login%20consent&response_type=code&idp_hint=${adaptateurEnvironnement.fournisseurIdentiteSuggere()}&client_id=${identifiantClient}&redirect_uri=${urlRedirectionConnexion}&state=${etat}&nonce=${nonce}${paramContexteMock}`);
 
   return stockeDansCookieSession({ etat }, adaptateurChiffrement, requete)
-    .then(() => construisURL())
+    .then(construisURL)
     .then((url) => reponse.render('redirectionNavigateur', { destination: url }));
 };
 
