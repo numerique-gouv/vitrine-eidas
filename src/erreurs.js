dda9eb824b5ebce96ddcca0c76705679752429d9
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-07-16 14:40:05 +0200

    [CORRECTION] N'affiche pas d'infos utilisateur si inexistantes
    Depuis le dernier commit, on ne vérifiait plus la présent du JWT session
    FC+ dans les infos utilisateurs (vu qu'il n'y était plus)… mais cette
    vérification permettait de s'assurer qu'on n'affichait pas d'infos
    utilisateur si le prénom et nom d'usage était inexistants (cas où
    l'utilisateur revient sur la page du site vitrine en appuyant sur le
    bouton « retour arrière » du navigateur).
    
    Ce commit corrige la régression.
    
    Co-authored-by: Fabien Lamarque <fabien.lamarque@beta.gouv.fr>

--- src/erreurs.js
+++ src/erreurs.js
@@ -1,6 +1,7 @@
 class ErreurAbsenceReponseDestinataire extends Error {}
 class ErreurAucunMessageDomibusRecu extends Error {}
 class ErreurDestinataireInexistant extends Error {}
+class ErreurDonneeManquante extends Error {}
 class ErreurEchecAuthentification extends Error {}
 class ErreurInstructionSOAPInconnue extends Error {}
 class ErreurJetonInvalide extends Error {}
@@ -10,6 +11,7 @@ module.exports = {
   ErreurAbsenceReponseDestinataire,
   ErreurAucunMessageDomibusRecu,
   ErreurDestinataireInexistant,
+  ErreurDonneeManquante,
   ErreurEchecAuthentification,
   ErreurInstructionSOAPInconnue,
   ErreurJetonInvalide,
--- test/modeles/utilisateur.spec.js
+++ test/modeles/utilisateur.spec.js
@@ -1,8 +1,19 @@
+const { ErreurDonneeManquante } = require('../../src/erreurs');
 const Utilisateur = require('../../src/modeles/utilisateur');
 
 describe("L'utilisateur courant", () => {
   it("sait s'afficher", () => {
     const utilisateur = new Utilisateur({ prenom: 'Juliette', nomUsage: 'Haucourt' });
     expect(utilisateur.afficheToi()).toBe('Juliette Haucourt');
+  });
+
+  it("vérifie qu'il est initialisé avec un nom d'usage et un prénom", () => {
+    expect.assertions(1);
+
+    try {
+      new Utilisateur({});
+    } catch (e) {
+      expect(e).toBeInstanceOf(ErreurDonneeManquante);
+    }
   });
 });
