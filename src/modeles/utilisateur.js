1a83b2864ef77d4cf529b66e5e1cadda50726ab1
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-07-16 14:40:05 +0200

    [CORRECTION] N'affiche pas d'infos utilisateur si inexistantes
    Depuis le dernier commit, on ne vérifiait plus la présent du JWT session
    FC+ dans les infos utilisateurs (vu qu'il n'y était plus)… mais cette
    vérification permettait de s'assurer qu'on n'affichait pas d'infos
    utilisateur si le prénom et nom d'usage était inexistants (cas où
    l'utilisateur revient sur la page du site vitrine en appuyant sur le
    bouton « retour arrière » du navigateur).
    
    Ce commit corrige la régression.
    
    Co-authored-by: Fabien Lamarque <fabien.lamarque@beta.gouv.fr>

--- src/modeles/utilisateur.js
+++ src/modeles/utilisateur.js
@@ -1,5 +1,11 @@
+const { ErreurDonneeManquante } = require('../erreurs');
+
 class Utilisateur {
   constructor(donnees) {
+    if (typeof donnees.prenom === 'undefined' && typeof donnees.nomUsage === 'undefined') {
+      throw new ErreurDonneeManquante("Prénom et nom d'usage obligatoires pour instancier un utilisateur");
+    }
+
     this.prenom = donnees.prenom;
     this.nomUsage = donnees.nomUsage;
   }
