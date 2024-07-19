38c20a076ce949b02b4240ebb3e56a0b5e5fb818
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-07-19 14:42:15 +0200

    [NETTOYAGE] Supprime code inutilisé

--- src/routes/utils.js
+++
@@ -1,5 +0,0 @@
-const stockeDansCookieSession = (infos, adaptateurChiffrement, requete) => adaptateurChiffrement
-  .genereJeton(infos)
-  .then((jwt) => { requete.session.jeton = jwt; });
-
-module.exports = { stockeDansCookieSession };
