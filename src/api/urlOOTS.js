42b755d46e59c0b5c822d29df2dce6ee464b16f1
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-08-12 17:35:39 +0200

    [CORRECTION] supprime `&` superflu

--- src/api/urlOOTS.js
+++ src/api/urlOOTS.js
@@ -1,5 +1,5 @@
 const urlOOTS = (adaptateurEnvironnement) => {
-  const urlOOTSRequete = `${adaptateurEnvironnement.urlBaseOOTSFrance()}/requete/pieceJustificative?codeDemarche=00&&codePays=FR`;
+  const urlOOTSRequete = `${adaptateurEnvironnement.urlBaseOOTSFrance()}/requete/pieceJustificative?codeDemarche=00&codePays=FR`;
   return adaptateurEnvironnement.avecOOTS() && urlOOTSRequete;
 };
 
