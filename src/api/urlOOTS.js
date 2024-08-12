4b9c47c65bee0b612981cbdd34839c904e0e0b69
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-08-12 17:45:04 +0200

    Injecte jeton accès dans URL requête OOTS

--- src/api/urlOOTS.js
+++ src/api/urlOOTS.js
@@ -1,5 +1,6 @@
-const urlOOTS = (adaptateurEnvironnement) => {
-  const urlOOTSRequete = `${adaptateurEnvironnement.urlBaseOOTSFrance()}/requete/pieceJustificative?codeDemarche=00&codePays=FR`;
+const urlOOTS = (adaptateurEnvironnement, requete) => {
+  const { jetonAcces } = requete.session;
+  const urlOOTSRequete = `${adaptateurEnvironnement.urlBaseOOTSFrance()}/requete/pieceJustificative?codeDemarche=00&codePays=FR&jetonAcces=${jetonAcces}`;
   return adaptateurEnvironnement.avecOOTS() && urlOOTSRequete;
 };
 
