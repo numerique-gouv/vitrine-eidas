c301d8a97d1eb6dc883d9db713c634b72266866a
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2024-12-02 14:55:45 +0100

    [NETTOYAGE] Supprime duplication
    … et protège la route `GET /oots/callback` par une variable
    d'environnement au passage.

--- src/routes/routesOOTS.js
+++ src/routes/routesOOTS.js
@@ -6,23 +6,13 @@ const routesOOTS = (config) => {
   const { adaptateurEnvironnement, depotDonnees } = config;
   const routes = express.Router();
 
-  routes.get('/document', (requete, reponse) => {
-    if (adaptateurEnvironnement.avecOOTS()) {
-      depotDonnees.demarreRecuperationDocument()
-        .then(() => reponse.render('redirectionNavigateur', { destination: urlOOTS(adaptateurEnvironnement, requete) }));
-    } else {
-      reponse.status(501).send('Not Implemented Yet!');
-    }
-  });
+  routes.get('/document', (requete, reponse) => depotDonnees
+    .demarreRecuperationDocument()
+    .then(() => reponse.render('redirectionNavigateur', { destination: urlOOTS(adaptateurEnvironnement, requete) })));
 
-  routes.post('/document', (requete, reponse) => {
-    if (adaptateurEnvironnement.avecOOTS()) {
-      depotDonnees.termineRecuperationDocument(Buffer.from(requete.body.document))
-        .then(() => reponse.send());
-    } else {
-      reponse.status(501).send('Not Implemented Yet!');
-    }
-  });
+  routes.post('/document', (requete, reponse) => depotDonnees
+    .termineRecuperationDocument(Buffer.from(requete.body.document))
+    .then(() => reponse.send()));
 
   routes.get('/callback', (requete, reponse) => {
     reponse.render('redirectionNavigateur', { destination: '/' });
